012a7d137d0de87e9c82fc76233f8233a8375bda
Gudekwizera <bertrand.sihimwe@gmail.com>
2023-02-17 11:50:39 -0800

    my first c

---
+++ 0x00-hello_world/1-program.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/** main = ENtry point
+ * Mystring = variable to hold a text
+ * return = always 0 (success)
+ */
+
+int main(void)
+
+{
+  char Mystring[]="Programming is like building a multilingual puzzle";
+
+  puts(Mystring);
+
+  return (0);
+  
+}
