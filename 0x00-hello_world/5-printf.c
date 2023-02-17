08042cae03692592afe0670840aa2face5f88307
Gudekwizera <bertrand.sihimwe@gmail.com>
2023-02-17 12:20:26 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+/**
+ *File =5-printf.c
+ *Author : Kwizera Bertrand <alxschool>
+ *
+ */
+
+#include<stdio.h>
+
+/** 
+ * main -Print "with proper grammar, but the outcome is a piece of art"
+ *
+ * Return : Always 0
+ *
+ */
+
+int main(void)
+{
+ 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+  	return(0);
+}
