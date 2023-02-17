6029831213878b7c4fb9a052bf00ce87c5b2bf4c
Gudekwizera <bertrand.sihimwe@gmail.com>
2023-02-17 12:02:34 -0800

    my first c-program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-gcc 1-program.c -o 2-file 
+#include<stdio.h>
+
+/** main = Entry point
+ * Description : program that print  "Programming is like building a multilingual puzzle" with puts function
+ * return : always 0 (success)
+ */
+
+int main(void)
+
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+  
+}
