e132577af4dc52b7fdc6098ebe62fb48dda01f72
Gudekwizera <bertrand.sihimwe@gmail.com>
2023-02-17 12:06:34 -0800

    betty style applied

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 
-/** main = Entry point
+/**
+ * main = Entry point
+ *
  * Description : program that print  "Programming is like building a multilingual puzzle" with puts function
+ *
  * return : always 0 (success)
+ *
  */
 
 int main(void)
 
 {
   
-  puts("\"Programming is like building a multilingual puzzle");
+          puts("\"Programming is like building a multilingual puzzle");
 
-  return (0);
+          return (0);
   
 }
