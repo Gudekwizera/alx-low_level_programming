ad256e26828fc29c7ddb85e438057abb97e124e6
Gudekwizera <bertrand.sihimwe@gmail.com>
2023-02-17 12:40:57 -0800

    assembly

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/**main - Entry point
- * return : Always 0 (success)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(void);
-
+int main(void)
 {
-	return(0);
+    return (0);
 }
