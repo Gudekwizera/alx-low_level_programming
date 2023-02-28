1caabba03094e0464073a5e82a077ef7f74c737f
Gudekwizera <bertrand.sihimwe@gmail.com>
2023-02-28 11:27:57 -0800

    fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,11 +14,11 @@ void rev_string(char *s)
 	while (s[index++])
 	len++;
 
-	for (index = len-1; index  >=  len / 2; index--)
+	for (index = len - 1; index  >=  len / 2; index--)
 	{
 		aux = s[index];
 		s[index] = s[len - index - 1];
-		s[len -index -1] = aux;
+		s[len - index -1] = aux;
 	}
 
 }
