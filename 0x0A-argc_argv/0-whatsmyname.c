23a875a38159b8f1ebc817d81d16dffb36ef64ff
Gudekwizera <bertrand.ishimwe@gmail.com>
2023-06-10 00:31:04 -0700

    tried new method

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,7 @@
  * @argv: An array of size argc
  * Return: always successful
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
-	return (0);
+	printf("%s\n", *argv);
 }
