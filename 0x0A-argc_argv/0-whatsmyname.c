0cd491a436b8774855d34bd3d7b1bbd6c1ab8010
Gudekwizera <bertrand.ishimwe@gmail.com>
2023-06-10 00:20:58 -0700

    fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
  * main - program to print name of program
  * @argc: size of argv array and number of command line arguments
  * @argv: An array of size argc
- * Return: Success
+ * Return: always successful
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
