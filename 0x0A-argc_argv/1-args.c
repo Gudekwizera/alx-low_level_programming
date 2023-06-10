d370a5d8b4a3d16b05110f9d5fe5f7909bb03f9b
Gudekwizera <bertrand.ishimwe@gmail.com>
2023-06-10 01:54:42 -0700

    fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: An array of size argc
  * Return: success
  */
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
